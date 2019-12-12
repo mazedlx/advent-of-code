dc7aaa5a6d4c58ebdf66a8955b92b8cd37d5093b
Christian Leo-Pernold <mazedlx@gmail.com>
2019-12-12 21:54:43 +0100

    Add 2015 Day 17.

--- 2015/day-17/part-02.php
+++ 2015/day-17/part-02.php
@@ -22,10 +22,13 @@ $containers = [
 	50,
 	6,
 ];
-
 $max = 150;
 $count = 0;
 $filled = [];
+$sizes = [];
+for ($i = 1; $i <= count($containers); $i++) {
+	$sizes[$i] = 0;
+}
 for($i = 0; $i < 2**count($containers) ; $i++){
 	foreach(str_split(strrev(decbin($i))) as $key => $digit) {
 		if ($digit == 1) {
@@ -35,9 +38,14 @@ for($i = 0; $i < 2**count($containers) ; $i++){
 	if(array_reduce($filled, function($carry, $item) {
 		return $carry + $item;
 	}, 0) === $max) {
+		$sizes[count($filled)] += 1;
 		$count++;
 	}
 	$filled = [];
 }
-
-echo $count, PHP_EOL;
+$sizes = array_filter($sizes, function($item) {
+	return $item > 0;
+});
+$sizes = array_values($sizes);
+sort($sizes);
+echo $sizes[0], PHP_EOL;
